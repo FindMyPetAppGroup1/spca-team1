a3f864ca80a5788fd7bdaf0ea23c1ad2a6213f67
Zain Amarshi <zamarshi1@gmail.com>
2016-11-19 15:31:29 -0800

    Revert "Add json to reports"

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-
-  def index
-    # this will render views/home/index.html.erb and it will use
-    # views/layouts/application.html.erb
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119202438) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "reports", force: :cascade do |t|
-    t.string   "name"
-    t.string   "pet_type"
-    t.string   "breed"
-    t.string   "age"
-    t.string   "color"
-    t.string   "sex"
-    t.string   "last_seen_date"
-    t.string   "last_seen_address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.text     "note"
-    t.string   "report_type"
-    t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "photo1"
-    t.string   "photo2"
-    t.string   "photo3"
-    t.index ["user_id"], name: "index_reports_on_user_id", using: :btree
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "uid"
-    t.string   "provider"
-    t.string   "oauth_token"
-    t.string   "oauth_secret"
-    t.string   "avatar"
-    t.text     "oauth_raw_data"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", using: :btree
-  end
-
-  add_foreign_key "reports", "users"
 end
