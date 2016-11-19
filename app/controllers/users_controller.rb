fbd25ffa82ca945884787a3fd1756b381f983a35
Zain Amarshi <zamarshi1@gmail.com>
2016-11-18 21:01:26 -0800

    Make user model, added cancancan gem, made sessions/users controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def  create
+    user_params = params.require(:user).permit([:first_name,
+                                                :last_name,
+                                                :email,
+                                                :password,
+                                                :password_confirmation])
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Thank you for signing up'
+    else
+      render :new
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161119033753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  end
 
 end
