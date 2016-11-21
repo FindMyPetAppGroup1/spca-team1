ebb6de6a5016ec6d121211a64c76a2a225cd16ec
Jeff Stock <jeffcstock@gmail.com>
2016-11-21 11:21:37 -0800

    change production environment to work with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.assets.digest = false
+  config.assets.digest = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
