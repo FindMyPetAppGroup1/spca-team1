2e89a8e14e46b3e578535072301279fb177378e7
cgsimmons <chris.simmons.alt@gmail.com>
2016-12-01 20:20:08 -0800

    Force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  Rails.application.configure do
+      config.force_ssl = true
+  end
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.assets.digest = true
